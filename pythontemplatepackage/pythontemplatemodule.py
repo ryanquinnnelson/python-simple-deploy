bfa5f82b6fa22ac8f44abe101f4243cccc4e389f
ryanquinnnelson <ryan.q.nelson@gmail.com>
2020-08-25 23:22:24 -0400

    Added documentation to get_id(), added save_id() function in anticipation of adding unit tests to the template

--- pythontemplatepackage/pythontemplatemodule.py
+++ pythontemplatepackage/pythontemplatemodule.py
@@ -7,7 +7,8 @@ print('Imported pythontemplatemodule!')
 
 def get_id(name):
     """
-    Returns the id associated with the given name.
+    Returns the id associated with the given name. Throws a ValueError if the given name is not John or Jane, for the
+    sake of writing a unit test example involving an exception.
 
     :param name: The first name of the individual
     :return: Integer
@@ -18,6 +19,18 @@ def get_id(name):
         return 456
     else:
         raise ValueError("my message")
+
+
+def save_id(id_number, filename):
+    """
+    Saves a given id in a file with the specified name. Allows for the writing of a unit test involving the temporary
+    directory available in pytest.
+    :param id_number:
+    :param filename:
+    :return:
+    """
+    with open(filename, 'w') as f:
+        f.write(id_number)
 
 
 def main():
