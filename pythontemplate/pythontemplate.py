b852bc90027ec1ac04ac71029cdada325600a81f
ryanquinnnelson <ryan.q.nelson@gmail.com>
2020-08-25 21:31:53 -0400

    Working function

--- pythontemplate/pythontemplate.py
+++ pythontemplate/pythontemplate.py
@@ -5,3 +5,22 @@ import requests
 # check whether runtime environment is a virtual environment
 print(platform.python_version())
 print(requests.__version__)
+
+# use requests
+r = requests.get('https://api.github.com/events')
+print(r.status_code)
+
+
+def get_id(name):
+    """
+    Returns the id associated with the given name.
+
+    :param name: The first name of the individual
+    :return: Integer
+    """
+    if name == "John":
+        return 123
+    elif name == "Jane":
+        return 456
+    else:
+        raise ValueError("my message")
