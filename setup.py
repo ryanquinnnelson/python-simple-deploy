8208892b775ba9140a17d5f4efef2f1327244d80
ryanquinnnelson <ryan.q.nelson@gmail.com>
2020-08-26 18:33:06 -0400

    Fixed packages value in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
+"""
+Follows https://godatadriven.com/blog/a-practical-guide-to-using-setup-py/
+"""
 from setuptools import setup, find_packages
 
 setup(
     name='pythontemplate-rnelson5',
     version='0.1.0',
-    packages=find_packages(include=['pythontemplatepackage', 'pythontemplatemodule.*']),
+    packages=find_packages(include=['pythontemplatepackage', 'pythontemplatepackage.*']),
     install_requires=['requests'],
     entry_points={'console_scripts': ['run-pythontemplate=pythontemplatepackage.pythontemplatemodule:main']}
 )
